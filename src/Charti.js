52af6d5bdeb24b186fbcc127f6845d6b6398e706
screeeen <miguel@screenimplosion.com>
2021-03-14 22:37:16 +0100

    some struct on winners

--- src/Charti.js
+++ src/Charti.js
@@ -1,38 +1,11 @@
 import React from 'react';
-import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { ScatterChart, Scatter, XAxis, YAxis,ZAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import {skateFem,skateMen,miniRamp,bmxParkMen,bmxParkFem} from './data/winners'
 
-// const data = [
-//   { x: 100, y: 200, z: 200 },
-//   { x: 120, y: 100, z: 260 },
-//   { x: 170, y: 300, z: 400 },
-//   { x: 140, y: 250, z: 280 },
-//   { x: 150, y: 400, z: 500 },
-//   { x: 110, y: 280, z: 200 },
-// ];
 
-  	 const data = [
-		{year: 2001, winner:  "Pay Álvarez (ESP)"},
-		{year: 2002, winner:  "Adrian Otero (ESP)"},
-		{year: 2003, winner:  "Raphael Brunis (FRA)"},
-		{year: 2004, winner:  "Marcos Chami (ARG)"},
-		{year: 2005, winner:  "David Lougedo (ESP)"},
-		{year: 2006, winner:  "Orlando Acosta (ESP)"},
-		{year: 2007, winner:  "Milton Martínez (ARG)"},
-		{year: 2008, winner:  "Milton Martínez (ARG)"},
-		{year: 2009, winner:  "Roger Silva (BRA)"},
-		{year: 2010, winner:  "Milton Martínez (ARG)"},
-		{year: 2011, winner:  "Cristian Estrada (COL)"},
-		{year: 2012, winner:  "Jorge Simoes (POR)"},
-		{year: 2013, winner:  "Danny Leon (ESP)"},
-		{year: 2014, winner:  "Kelvin Hoefler (BRA)"},
-		{year: 2015, winner:  "Aurelien Giraud (FRA)"},
-		{year: 2016, winner:  "Danny Leon (ESP)"},
-		{year: 2017, winner:  "Aurelien Giraud (FRA)"},
-		{year: 2018, winner:  "Gustavo Ribeiro (POR)"},
-		{year: 2019, winner:  "Ivan Monteiro (BRA)}"},
-       ]
 
 export const Charti = () => {
+
     return (
       <ResponsiveContainer>
         <ScatterChart
@@ -45,11 +18,16 @@ export const Charti = () => {
             left: 20,
           }}
         >
-          <CartesianGrid horizontal={false}/>
-          <YAxis type="number" dataKey="year" name="year" domain={data.map(y=>y.year)} tickCount={19} interval={0}/>
-          <XAxis type="category" dataKey="winner" interval={-1}/>
+          {/* <CartesianGrid horizontal={false}/> */}
+          <YAxis type="number" dataKey="year" domain={[2001,2019]}  tickCount={19} interval={0}/>
+          <XAxis type="number" dataKey="index" label={{value:"skate men"}} />
+          <ZAxis type="category" dataKey="skateMen" interval={0}/>
           <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-          <Scatter name="competition" data={data} fill="#8884d8" />
+          <Scatter name="skateMen" data={skateMen} fill="#8884d8" />
+          <Scatter name="skateFem" data={skateFem} fill="#8884d8" />
+          <Scatter name="miniRamp" data={miniRamp} fill="#8884d8" />
+          <Scatter name="bmxParkMen" data={bmxParkMen} fill="#8884d8" />
+          <Scatter name="bmxParkFem" data={bmxParkFem} fill="#8884d8" />
         </ScatterChart>
         </ResponsiveContainer>
     );
---
+++ src/data/winners.js
@@ -0,0 +1,159 @@
+export const skateMen = [
+	{year: 2001, index: 1, skateMen:  "Pay Álvarez (ESP)"},
+	{year: 2002, index: 1, skateMen:  "Adrian Otero (ESP)"},
+	{year: 2003, index: 1, skateMen:  "Raphael Brunis (FRA)"},
+	{year: 2004, index: 1, skateMen:  "Marcos Chami (ARG)"},
+	{year: 2005, index: 1, skateMen:  "David Lougedo (ESP)"},
+	{year: 2006, index: 1, skateMen:  "Orlando Acosta (ESP)"},
+	{year: 2007, index: 1, skateMen:  "Milton Martínez (ARG)"},
+	{year: 2008, index: 1, skateMen:  "Milton Martínez (ARG)"},
+	{year: 2009, index: 1, skateMen:  "Roger Silva (BRA)"},
+	{year: 2010, index: 1, skateMen:  "Milton Martínez (ARG)"},
+	{year: 2011, index: 1, skateMen:  "Cristian Estrada (COL)"},
+	{year: 2012, index: 1, skateMen:  "Jorge Simoes (POR)"},
+	{year: 2013, index: 1, skateMen:  "Danny Leon (ESP)"},
+	{year: 2014, index: 1, skateMen:  "Kelvin Hoefler (BRA)"},
+	{year: 2015, index: 1, skateMen:  "Aurelien Giraud (FRA)"},
+	{year: 2016, index: 1, skateMen:  "Danny Leon (ESP)"},
+	{year: 2017, index: 1, skateMen:  "Aurelien Giraud (FRA)"},
+	{year: 2018, index: 1, skateMen:  "Gustavo Ribeiro (POR)"},
+	{year: 2019, index: 1, skateMen:  "Ivan Monteiro (BRA)}"},
+   ]
+
+export const skateFem = [
+		{year: "2013", index: 2, skateFem: "Eugenia Ginepro (ARG)"},
+		{year: "2014", index: 2, skateFem: "Pamela Rosa (BRA)"},
+		{year: "2015", index: 2, skateFem: "Leticia Bufoni (BRA)"},
+		{year: "2016", index: 2, skateFem: "Julia Brueckler (AUT)"},
+		{year: "2017", index: 2, skateFem: "Candy Jacobs (HOL)"},
+		{year: "2018", index: 2, skateFem: "Candy Jacobs (HOL)"},
+		{year: "2019", index: 2, skateFem: "Keet Oldenbeuving (HOL)"}
+]
+
+export const miniRamp = [
+		{year:"2016", index: 3, miniRamp: "Danny Leon (ESP)"},
+		{year:"2017", index: 3, miniRamp: "Aurelien Giraud (FRA)"},
+		{year:"2018", index: 3, miniRamp: "Jake Ilardi (USA)"},
+		{year:"2019", index: 3, miniRamp: "Danny Leon (ESP)}"}
+	]
+	
+export const bmxParkMen = [
+		{year: "2007",index: 4, bmxParkMen: "Gabruel Mastragostino (ARG)"},
+		{year: "2008",index: 4, bmxParkMen: "Sergio Layos (ESP)"},
+		{year: "2009",index: 4, bmxParkMen: "Daniel Peñafiel (ESP)"},
+		{year: "2010",index: 4, bmxParkMen: "Sergio Layos (ESP)"},
+		{year: "2011",index: 4, bmxParkMen: "Daniel Peñafiel (ESP)"},
+		{year: "2012",index: 4, bmxParkMen: "Daniel Peñafiel (ESP)"},
+		{year: "2013",index: 4, bmxParkMen: "Daniel Dhers (VEN)"},
+		{year: "2014",index: 4, bmxParkMen: "Jonathan Camacho (COL)"},
+		{year: "2015",index: 4, bmxParkMen: "Daniel Dhers (VEN)"},
+		{year: "2016",index: 4, bmxParkMen: "Konstantin Andreev (RUS)"},
+		{year: "2017",index: 4, bmxParkMen: "Jonathan Camacho (COL)"},
+		{year: "2018",index: 4, bmxParkMen: "Daniel Dhers (VEN)"},
+		{year: "2019",index: 4, bmxParkMen: "Sergio Layos (ESP)"}
+]
+	
+export const bmxParkFem = [
+		{year: "2016", index: 5, bmxParkFem: "Katty Diaz (VEN)"},
+		{year: "2017", index: 5, bmxParkFem: "Camila Harambour (CHI)"},
+		{year: "2018", index: 5, bmxParkFem: "Teresa Fernández (ESP)"},
+		{year: "2019", index: 5, bmxParkFem: "Lara Lessmann (GER)"}
+]
+// 	"BMXStreet": {
+// 		"2017": "Broc Raiford (USA)",
+// 		"2018": "Courage Adams (ESP)",
+// 		"2019": "Courage Adams (ESP)"
+// 	},
+// 	"BMXFlatland": {
+// 		"2007": "Viki Gómez (ESP)",
+// 		"2008": "Viki Gómez (ESP)",
+// 		"2009": "",
+// 		"2010": "Matthias Dandois (FRA)",
+// 		"2011": "Matthias Dandois (FRA)",
+// 		"2012": "Matthias Dandois (FRA)",
+// 		"2013": "Matthias Dandois (FRA)",
+// 		"2014": "Viki Gómez (ESP)",
+// 		"2015": "Matthias Dandois (FRA)",
+// 		"2016": "Alex Jumelin (FRA)",
+// 		"2017": "Viki Gómez (ESP)",
+// 		"2018": "Viki Gómez (ESP)",
+// 		"2019": "Alberto Moya (ESP)"
+// 	},
+// 	"MTBDowntownM": {
+// 		"2010": "Paulo Domingues (POR)",
+// 		"2011": "Aaron Caballero (ESP)",
+// 		"2012": "José Borges (POR)",
+// 		"2013": "José Borges (POR)",
+// 		"2014": "José Borges (POR)",
+// 		"2015": "Aaron Caballero (ESP)",
+// 		"2016": "José Borges (POR)",
+// 		"2017": "Edgar Carballo (ESP)",
+// 		"2018": "Marco Veiga (ESP)",
+// 		"2019": "Edgar Carballo (ESP)"
+// 	},
+// 	"MTBDowntownF": {
+// 		"2010": "Rosi Martínez (ESP)",
+// 		"2011": "Eva Castro (ESP)",
+// 		"2012": "Rosi Martínez (ESP)",
+// 		"2013": "Rosi Martínez (ESP)",
+// 		"2014": "Rosi Martínez (ESP)",
+// 		"2015": "Rosi Martínez (ESP)",
+// 		"2016": "Rosi Martínez (ESP)",
+// 		"2017": "Eva Castro (ESP)",
+// 		"2018": "Eva Castro (ESP)",
+// 		"2019": "Eva Castro (ESP)"
+// 	},
+// 	"FMBDirtJump": {
+// 		"2011": "Andreu Llacondeguy (ESP)",
+// 		"2012": "Xavier Pasamonte (ESP)",
+// 		"2013": "Nicholi Rogatkin (USA)",
+// 		"2014": "Nicholi Rogatkin (USA)",
+// 		"2015": "Nicholi Rogatkin (USA)",
+// 		"2016": "Paul Couderc (FRA)",
+// 		"2017": "Nicholi Rogatkin (USA)",
+// 		"2018": "Lukas Huppert (SUI)",
+// 		"2019": "Tom Isted (UK)"
+// 	},
+// 	"BreakDance": {
+// 		"2007": "Gaiolin Roots (POR)",
+// 		"2008": "Iron Skulls (ESP)",
+// 		"2009": "Lunatiks (ESP)",
+// 		"2010": "Fusion Rockers (ESP)",
+// 		"2011": "Barna Squad (ESP)",
+// 		"2012": "Fusion Rockers (ESP)",
+// 		"2013": "Fusion Rockers (ESP)",
+// 		"2014": "Fusion Rockers (ESP)",
+// 		"2015": "Momentum Crew (POR)",
+// 		"2016": "Malaganzters (ESP)",
+// 		"2017": "Fusion Rockers (ESP)",
+// 		"2018": "Zoo Evolution Kings (POR)",
+// 		"2019": "XVII Generation (ESP)"
+// 	},
+// 	"Basket3x3": {
+// 		"2018": "Basket Gijón (ESP)",
+// 		"2019": "Titans (ESP)"
+// 	}
+// }
+
+
+// const data = [
+// 	{year: 2001, skateM:  "Pay Álvarez (ESP)"},
+// 	{year: 2002, skateM:  "Adrian Otero (ESP)"},
+// 	{year: 2003, skateM:  "Raphael Brunis (FRA)"},
+// 	{year: 2004, skateM:  "Marcos Chami (ARG)"},
+// 	{year: 2005, skateM:  "David Lougedo (ESP)"},
+// 	{year: 2006, skateM:  "Orlando Acosta (ESP)"},
+// 	{year: 2007, skateM:  "Milton Martínez (ARG)"},
+// 	{year: 2008, skateM:  "Milton Martínez (ARG)"},
+// 	{year: 2009, skateM:  "Roger Silva (BRA)"},
+// 	{year: 2010, skateM:  "Milton Martínez (ARG)"},
+// 	{year: 2011, skateM:  "Cristian Estrada (COL)"},
+// 	{year: 2012, skateM:  "Jorge Simoes (POR)"},
+// 	{year: 2013, skateF: "Eugenia Ginepro (ARG)", skateM:  "Danny Leon (ESP)"},
+// 	{year: 2014, skateF: "Pamela Rosa (BRA)", skateM:  "Kelvin Hoefler (BRA)"},
+// 	{year: 2015, skateF: "Leticia Bufoni (BRA)", skateM:  "Aurelien Giraud (FRA)"},
+// 	{year: 2016, skateMiniramp: "Danny Leon (ESP)", skateF: "Julia Brueckler (AUT)", skateM:  "Danny Leon (ESP)"},
+// 	{year: 2017, skateMiniramp: "Aurelien Giraud (FRA)", skateF: "Candy Jacobs (HOL)", skateM:  "Aurelien Giraud (FRA)"},
+// 	{year: 2018, skateMiniramp: "Jake Ilardi (USA)", skateF: "Candy Jacobs (HOL)", skateM:  "Gustavo Ribeiro (POR)"},
+// 	{year: 2019, skateMiniramp: "Danny Leon (ESP)", skateF: "Keet Oldenbeuving (HOL)", skateM:  "Ivan Monteiro (BRA)}"},
+// ]
