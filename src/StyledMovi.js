0c721aa3f7ae40d14d6d9840084d28ed8ec0f6f1
screeeen <miguel@screenimplosion.com>
2021-04-05 00:13:17 +0200

    test image sizes

--- src/StyledMovi.js
+++ src/StyledMovi.js
@@ -22,25 +22,25 @@ export const ColumnStyle = styled.div`
 `;
 
 export const Name = styled.div`
-    /* z-index: 10;  */
+    @media(min-width: 768px) {
     background: black;
     color: white;
     font-size: 0.6em;
     position: relative;
     bottom: 10px;
     padding: 5px;
+    }
 `;
 
 export const CellStyle = styled.div`
         display: flex;
         flex-flow: column;
-        width: 20px;
+        /* width: 20px; */
         height: 60px;
         padding: 1px;
         font-size: 0.4em;
         background: ${props => props.color % 2 ? '#EDF8' : '' };
-
-        /* border: 1px solid gray; */
+        z-index: -2; 
 
         /* BIG */
         @media(min-width: 768px) {
@@ -49,30 +49,15 @@ export const CellStyle = styled.div`
             padding: 6px;
             font-size: 1em;
             padding: 40px 20px;
-
-            /* border: 1px solid gray; */
+            z-index: -1; 
     }
-
     `;
 
-// export const CellYear = styled.div`
-//         display: flex;
-//         flex-flow: column;
-//         padding: 40% 10%;
-//         font-size: 1em;
-//         font-weight: bold;
-//         color: slateGray;
-//         @media(min-width: 768px) {
-//             width: 60px;
-//             height: 40px;
-//             /* border: 1px solid gray; */
-//             padding: 40% 10%;
-//     }
-//     `;
-
 export const ImgSt = styled.img`
-        width: 100%;
+        height: 100%;
         filter: grayscale(100%) drop-shadow(2px 2px 1px gray);
+        z-index: 1; 
+
     `;
     
     
