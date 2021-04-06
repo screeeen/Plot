ffbcde10a0d7c50fa5bf91285ac8a4605012a630
screeeen <miguel@screenimplosion.com>
2021-04-06 08:03:46 +0200

    WIP

--- src/Movi.js
+++ src/Movi.js
@@ -1,88 +1,46 @@
 import React, {useState,useEffect} from 'react';
 import {wins} from './data/winnersTable'
-import {GridStyle,ColumnStyle,Name,CellStyle,ImgSt} from './StyledMovi'
-
+import {GridStyle,RowStyle,Name,CellStyle,ImgSt, TitleCell} from './StyledMovi'
 export const Movi = () => {
-
     const [dataReversed,setData] = useState(null);
-
     useEffect(()=> {
         const rev = wins.reverse();
         setData(rev);
     },[])
-
     const generateImage = (url) => url && <ImgSt src={process.env.PUBLIC_URL + url} alt="*"/>;
-    
     const showTooltip = (info) => console.log(info);
-
     const printName = (name)=> name && <Name>{name}</Name>
-
     const cell = (i,name,pic) => <CellStyle key={i} color={i} onMouseOver={()=>showTooltip(name)}>{generateImage(pic)}{printName(name)}</CellStyle>
-    
-
     const generateCells = () => {
+        const objectModel = ['year','skateMen','skateWomen','miniRamp','bmxParkMen','bmxParkWomen','bmxStreet','flatLand','mtbDowntownMen','mtbDowntownWomen','fmbDirtJump','breakDance','basket3x3']
         console.log('reverseData',dataReversed)
             return (
                 <>
-                <ColumnStyle>
-                    <CellStyle></CellStyle>
-                    {dataReversed.map((year,i) => <CellStyle key={i}>{year.year}</CellStyle>)}
-                </ColumnStyle>
-                <ColumnStyle color={0}>
-                    <CellStyle>Sk8 Men</CellStyle>
-                    {dataReversed.map((year,i) => cell(i,year.skateMen,year.skateMenPic))}
-                </ColumnStyle>
-                <ColumnStyle color={1}>
-                    <CellStyle>Sk8 Women</CellStyle>
-                    {dataReversed.map((year,i) => cell(i,year.skateWomen,year.skateWomenPic))}
-                </ColumnStyle>
-                <ColumnStyle color={0}>
-                    <CellStyle>Mini Ramp</CellStyle>
-                    {dataReversed.map((year,i) => cell(i,year.miniramp,year.miniRampPic))}
-                </ColumnStyle>
-                <ColumnStyle color={1}>
-                    <CellStyle>BMX Park Men</CellStyle>
-                    {dataReversed.map((year,i) => cell(i,year.bmxParkMen,year.bmxParkMenPic))}
-                </ColumnStyle>
-                <ColumnStyle color={0}>
-                    <CellStyle>BMX Park Women</CellStyle>
-                    {dataReversed.map((year,i) => cell(i,year.bmxParkWomen,year.bmxParkWomenPic))}
-                </ColumnStyle>
-                <ColumnStyle color={1}>
-                    <CellStyle>BMX Street</CellStyle>
-                    {dataReversed.map((year,i) => cell(i,year.bmxStreet,year.bmxStreetPic))}
-                </ColumnStyle>
-                <ColumnStyle color={0}>
-                    <CellStyle>Flat Land</CellStyle>
-                    {dataReversed.map((year,i) => cell(i,year.flatland,year.flatLandPic))}
-                </ColumnStyle>
-                <ColumnStyle color={1}>
-                    <CellStyle>MTB Downtown Men</CellStyle>
-                    {dataReversed.map((year,i) => cell(i,year.mtbDowntownMen,year.mtbDowntownMenPic))}
-                </ColumnStyle>
-                <ColumnStyle color={0}>
-                    <CellStyle>MTB Downtown Women</CellStyle>
-                    {dataReversed.map((year,i) => cell(i,year.mtbDowntownWomen,year.mtbDowntownWomenPic))}
-                </ColumnStyle>
-                <ColumnStyle color={1}>
-                    <CellStyle>FMB Dirt Jump</CellStyle>
-                    {dataReversed.map((year,i) => cell(i,year.fmbDirtJump,year.fmbDirtJumpPic))}
-                </ColumnStyle>
-                <ColumnStyle color={0}>
-                    <CellStyle>Break Dance</CellStyle>
-                    {dataReversed.map((year,i) => cell(i,year.breakDance,year.breakDancePic))}
-                </ColumnStyle>
-                <ColumnStyle color={1}>
-                    <CellStyle>Basket 3x3</CellStyle>
-                    {dataReversed.map((year,i) => cell(i,year.basket3x3,year.basket3x3Pic))}
-                </ColumnStyle>
+                <RowStyle color={0}>
+                    <TitleCell></TitleCell>
+                    <TitleCell>Sk8 Men</TitleCell>
+                    <TitleCell>Sk8 Women</TitleCell>
+                    <TitleCell>Mini Ramp</TitleCell>
+                    <TitleCell>BMX Park Men</TitleCell>
+                    <TitleCell>BMX Park Women</TitleCell>
+                    <TitleCell>BMX Street</TitleCell>
+                    <TitleCell>Flat Land</TitleCell>
+                    <TitleCell>MTB Downtown Men</TitleCell>
+                    <TitleCell>MTB Downtown Women</TitleCell>
+                    <TitleCell>FMB Dirt Jump</TitleCell>
+                    <TitleCell>Break Dance</TitleCell>
+                    <TitleCell>Basket 3x3</TitleCell>
+                </RowStyle>
+                {dataReversed.map((yearResults,i) => 
+                    <RowStyle color={i} key={i}>
+                    {objectModel.map((model, i) => 
+                        model && model === 'year' ? <CellStyle key={i}>{yearResults[model]}</CellStyle> :
+                        cell(i,yearResults[model],yearResults[`${model}Pic`])
+                    )}
+                    </RowStyle>
+                )}
                 </>
             )
     }
-
     return <GridStyle>{dataReversed && generateCells()}</GridStyle>
-       
-      
-    
 }
-
--- src/StyledMovi.js
+++ src/StyledMovi.js
@@ -1,24 +1,25 @@
 import styled from 'styled-components'
-
 export const GridStyle = styled.div`
-    height:100vh;
     display: flex;
+    flex-direction: column;
     align-items: start;
     justify-content: start;
     margin: 2%;
     /* background: whitesmoke; */
-
     @media(min-width: 768px) {
         /* background: mediumseagreen;
         color: papayawhip; */
         justify-content: center;
     }
     `;
-    
-export const ColumnStyle = styled.div`
-    flex-flow: 'column';
-    align-items: start;
-    justify-content: start; 
+export const RowStyle = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    background: ${props => props.color % 2 ? '#EDF5' : '#EDF3' };
+    z-index: -1; 
 `;
 
 export const Name = styled.div`
@@ -29,36 +30,37 @@ export const Name = styled.div`
     position: relative;
     bottom: 10px;
     padding: 5px;
+    z-index: 4; 
+
     }
 `;
-
 export const CellStyle = styled.div`
         display: flex;
-        flex-flow: column;
+        flex-flow: column nowrap;
         /* width: 20px; */
         height: 60px;
-        padding: 1px;
+        padding: 6px;
+        width: 7.6%;
         font-size: 0.4em;
-        background: ${props => props.color % 2 ? '#EDF8' : '' };
-        z-index: -2; 
-
+        &:first-child{
+            justify-content: center;
+        }
         /* BIG */
         @media(min-width: 768px) {
             width: 60px;
             height: 60px;
             padding: 6px;
             font-size: 1em;
-            padding: 40px 20px;
             z-index: -1; 
     }
     `;
-
+export const TitleCell = styled(CellStyle)`
+    word-break: break-all;
+    height: 20px;
+`;
 export const ImgSt = styled.img`
         height: 100%;
-        width:100%;
+        /* width: 100%; */
         filter: grayscale(100%) drop-shadow(2px 2px 1px gray);
         z-index: 1; 
-
     `;
-    
-    
