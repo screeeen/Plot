8db45f02ef871b9cecc864a9b45550c11673597d
screeeen <miguel@screenimplosion.com>
2021-04-16 19:41:22 +0200

    order change

--- src/Movi.js
+++ src/Movi.js
@@ -1,47 +1,93 @@
-import React, {useState,useEffect} from 'react';
-import {wins} from './data/winnersTable'
-import {GridStyle,RowStyle,Name,CellStyle,ImgSt, TitleCell} from './StyledMovi'
+import React, { useState, useEffect } from "react";
+import { wins } from "./data/winnersTable";
+import {
+  GridStyle,
+  ColumnStyle,
+  Name,
+  CellStyle,
+  ImgSt,
+  TitleCell,
+} from "./StyledMovi";
 
 export const Movi = () => {
-    const [dataReversed,setData] = useState(null);
-    useEffect(()=> {
-        const rev = wins.reverse();
-        setData(rev);
-    },[])
-    const generateImage = (url) => url && <ImgSt src={process.env.PUBLIC_URL + url} alt="*"/>;
-    const showTooltip = (info) => console.log(info);
-    const printName = (name)=> name && <Name>{name}</Name>
-    const cell = (i,name,pic) => <CellStyle key={i} color={i} onMouseOver={()=>showTooltip(name)}>{generateImage(pic)}{printName(name)}</CellStyle>
-    const generateCells = () => {
-        const objectModel = ['year','skateMen','skateWomen','miniRamp','bmxParkMen','bmxParkWomen','bmxStreet','flatLand','mtbDowntownMen','mtbDowntownWomen','fmbDirtJump','breakDance','basket3x3']
-        console.log('reverseData',dataReversed)
-            return (
-                <>
-                <RowStyle color={0}>
-                    <TitleCell></TitleCell>
-                    <TitleCell>Sk8 Men</TitleCell>
-                    <TitleCell>Sk8 Women</TitleCell>
-                    <TitleCell>Mini Ramp</TitleCell>
-                    <TitleCell>BMX Park Men</TitleCell>
-                    <TitleCell>BMX Park Women</TitleCell>
-                    <TitleCell>BMX Street</TitleCell>
-                    <TitleCell>Flat Land</TitleCell>
-                    <TitleCell>MTB Downtown Men</TitleCell>
-                    <TitleCell>MTB Downtown Women</TitleCell>
-                    <TitleCell>FMB Dirt Jump</TitleCell>
-                    <TitleCell>Break Dance</TitleCell>
-                    <TitleCell>Basket 3x3</TitleCell>
-                </RowStyle>
-                {dataReversed.map((yearResults,i) => 
-                    <RowStyle color={i} key={i}>
-                    {objectModel.map((model, i) => 
-                        model && model === 'year' ? <CellStyle key={i}>{yearResults[model]}</CellStyle> :
-                        cell(i,yearResults[model],yearResults[`${model}Pic`])
-                    )}
-                    </RowStyle>
-                )}
-                </>
-            )
-    }
-    return <GridStyle>{dataReversed && generateCells()}</GridStyle>
-}
+  const [dataReversed, setData] = useState(null);
+  useEffect(() => {
+    // const rev = wins.reverse();
+    const rev = wins;
+    setData(rev);
+  }, []);
+  const generateImage = (url) =>
+    url && <ImgSt src={process.env.PUBLIC_URL + url} alt="*" />;
+  const showTooltip = (info) => console.log(info);
+  const printName = (name) => name && <Name>{name}</Name>;
+  const cell = (i, name, pic) => (
+    <CellStyle key={i} color={i} onMouseOver={() => showTooltip(name)}>
+      {generateImage(pic)}
+      {printName(name)}
+    </CellStyle>
+  );
+  const generateCells = () => {
+    const objectModel = [
+      "year",
+      "skateMen",
+      "skateWomen",
+      "miniRamp",
+      "bmxParkMen",
+      "bmxParkWomen",
+      "bmxStreet",
+      "flatLand",
+      "mtbDowntownMen",
+      "mtbDowntownWomen",
+      "fmbDirtJump",
+      "breakDance",
+      "basket3x3",
+    ];
+    console.log("reverseData", dataReversed);
+    return (
+      <>
+        <ColumnStyle color={0}>
+          <TitleCell></TitleCell>
+          <TitleCell>Sk8 Men</TitleCell>
+          <TitleCell>Sk8 Women</TitleCell>
+          <TitleCell>Mini Ramp</TitleCell>
+          <TitleCell>BMX Park Men</TitleCell>
+          <TitleCell>BMX Park Women</TitleCell>
+          <TitleCell>BMX Street</TitleCell>
+          <TitleCell>Flat Land</TitleCell>
+          <TitleCell>MTB Down town Men</TitleCell>
+          <TitleCell>MTB Down town Women</TitleCell>
+          <TitleCell>FMB Dirt Jump</TitleCell>
+          <TitleCell>Break Dance</TitleCell>
+          <TitleCell>Basket 3x3</TitleCell>
+        </ColumnStyle>
+        {dataReversed.map((yearResults, i) => (
+          <ColumnStyle color={i} key={i}>
+            {objectModel.map((model, i) =>
+              model && model === "year" ? (
+                <CellStyle key={i}>{yearResults[model]}</CellStyle>
+              ) : (
+                cell(i, yearResults[model], yearResults[`${model}Pic`])
+              )
+            )}
+          </ColumnStyle>
+        ))}
+        <ColumnStyle color={0}>
+          <TitleCell></TitleCell>
+          <TitleCell>Sk8 Men</TitleCell>
+          <TitleCell>Sk8 Women</TitleCell>
+          <TitleCell>Mini Ramp</TitleCell>
+          <TitleCell>BMX Park Men</TitleCell>
+          <TitleCell>BMX Park Women</TitleCell>
+          <TitleCell>BMX Street</TitleCell>
+          <TitleCell>Flat Land</TitleCell>
+          <TitleCell>MTB Down town Men</TitleCell>
+          <TitleCell>MTB Down town Women</TitleCell>
+          <TitleCell>FMB Dirt Jump</TitleCell>
+          <TitleCell>Break Dance</TitleCell>
+          <TitleCell>Basket 3x3</TitleCell>
+        </ColumnStyle>
+      </>
+    );
+  };
+  return <GridStyle>{dataReversed && generateCells()}</GridStyle>;
+};
--- src/StyledMovi.js
+++ src/StyledMovi.js
@@ -1,86 +1,87 @@
-import styled from 'styled-components'
-
+import styled from "styled-components";
 
 export const GridStyle = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: start;
-    /* justify-content: start; */
-    justify-content: center;
+  /* width: 800px; */
+  display: flex;
+  flex-direction: row;
+  align-items: start;
+  /* justify-content: start; */
+  /* justify-content: center; */
 
-    margin: 4%;
-    /* width: 800px; */
+  margin: 4%;
+  /* width: 800px; */
+  box-sizing: border-box;
+
+  /* BIG */
+  @media (min-width: 768px) {
     box-sizing: border-box;
+    /* justify-content: center; */
+    /* margin: 10%; */
+    /* width: 960px; */
+  }
+`;
 
-    /* BIG */
-    @media(min-width: 768px) {
-        box-sizing:border-box;
-        justify-content: center;
-        margin: 10%;
-        /* width: 960px; */
-    }
-    `;
-
-export const RowStyle = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    width: 100%;
-    /* background: ${props => props.color % 2 ? '#EDF5' : '#EDF3' }; */
-    z-index: -1; 
+export const ColumnStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  /* background: ${(props) => (props.color % 2 ? "#FFF" : "#EFF3")}; */
+  z-index: -1;
 `;
 
 export const Name = styled.div`
-    @media(min-width: 768px) {
+  @media (min-width: 768px) {
     font-size: 0.6em;
-    z-index: 4; 
-
-    }
+    z-index: 4;
+  }
 `;
 export const CellStyle = styled.div`
-        display: flex;
-        flex-flow: column nowrap;
-        height: 60px;
-        padding: 12px;
-        width: 7.6%;      
-        min-width: 7.6%;      
-        font-size: 0.4em;
-        border: 1px solid lightgray;
+  display: flex;
+  flex-flow: column nowrap;
+  height: 80px;
+  min-height: 120px;
+  padding: 2px;
+  width: 60px;
+  /* width: 7.6%; */
+  /* min-width: 7.6%; */
+  font-size: 0.4em;
+  /* border: 1px solid lightgray; */
 
-        /* YEAR */
-        &:first-child{
-            font-stretch: condensed;
-            justify-content: center;
-            font-family: 'Stint Ultra Condensed', cursive;
-            font-size: 2em;
-            padding: 0px;
-
-        }
-        /* BIG */
-        @media(min-width: 768px) {
-            /* padding: 20px; */
-            height: 100%;
-            min-width: 7.6%;        
-            font-size: 1em;
-            z-index: -1; 
-    }
-    `;
+  /* YEAR */
+  &:first-child {
+    font-stretch: condensed;
+    justify-content: center;
+    font-family: "Stint Ultra Condensed", cursive;
+    font-size: 2.5em;
+    padding: 0px;
+  }
+  /* BIG */
+  @media (min-width: 768px) {
+    /* padding: 20px; */
+    height: 100%;
+    min-width: 7.6%;
+    font-size: 1em;
+    z-index: -1;
+  }
+`;
 export const TitleCell = styled(CellStyle)`
-    /* word-break: break-all; */
-    word-break: normal;
-    height: 20px;
-    font-family: 'Stint Ultra Condensed', cursive;
-    /* font-size: 1.2em; */
+  /* word-break: break-all; */
+  word-break: normal;
+  /* height: 230px; */
+  height: 80px;
+  min-height: 120px;
+  font-family: "Stint Ultra Condensed", cursive;
+  font-size: 2em;
 `;
 export const ImgSt = styled.img`
-        height: 80%;
-        /* width: 100%; */
-        filter: grayscale(100%) drop-shadow(2px 2px 1px gray);
-        z-index: 1; 
+  height: 80%;
+  /* width: 100%; */
+  filter: grayscale(100%) drop-shadow(2px 2px 1px gray);
+  z-index: 1;
 
-        @media(min-width: 768px) {
-            height: 100%;
-
-        }
-    `;
+  @media (min-width: 768px) {
+    height: 100%;
+  }
+`;
