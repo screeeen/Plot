2929a40555a31dbc5fbcb6ebe86731f97540560c
screeeen <miguel@screenimplosion.com>
2025-04-26 13:53:03 +0200

    sube

--- src/Movi.js
+++ src/Movi.js
@@ -1,64 +1,60 @@
-import React, { useState, useEffect } from "react";
-import { wins } from "./data/winnersTable";
-import { GridStyle, TitleCell, Name, CellStyle, ImgSt } from "./StyledMovi";
+import React, { useState, useEffect } from 'react';
+import { wins } from './data/winnersTable';
+import { GridStyle, TitleCell, Name, CellStyle, ImgSt } from './StyledMovi';
 
 export const Movi = () => {
-  const [dataReversed, setData] = useState(null);
-  useEffect(() => {
-    // const rev = wins.reverse();
-    const rev = wins;
-    setData(rev);
-  }, []);
+	const [dataReversed, setData] = useState(null);
+	useEffect(() => {
+		// const rev = wins.reverse();
+		const rev = wins;
+		setData(rev);
+	}, []);
 
-  const generateImage = (url) =>
-    url && <ImgSt src={process.env.PUBLIC_URL + url} alt="*" />;
+	const generateImage = (url) => url && <ImgSt src={process.env.PUBLIC_URL + url} alt='*' />;
 
-  const showTooltip = (info) => console.log(info);
+	const showTooltip = (info) => console.log(info);
 
-  const printName = (name) => name && <Name>{name}</Name>;
+	const printName = (name) => name && <Name>{name}</Name>;
 
-  const cell = (i, name, pic) => (
-    <CellStyle key={name + pic} color={i} onMouseOver={() => showTooltip(name)}>
-      {generateImage(pic)}
-      {printName(name)}
-    </CellStyle>
-  );
-  const generateCells = () => {
-    const objectModel = [
-      // "year",
-      // "skateMen",
-      "skateWomen",
-      // "miniRamp",
-      // "bmxParkMen",
-      // "bmxParkWomen",
-      // "bmxStreet",
-      // "flatLand",
-      // "mtbDowntownMen",
-      // "mtbDowntownWomen",
-      // "fmbDirtJump",
-      // "breakDance",
-      // "basket3x3",
-    ];
+	const cell = (i, name, pic) => (
+		<CellStyle key={name + pic} color={i} onMouseOver={() => showTooltip(name)}>
+			{generateImage(pic)}
+			{printName(name)}
+		</CellStyle>
+	);
+	const generateCells = () => {
+		const objectModel = [
+			// "year",
+			'skateMen',
+			// "skateWomen",
+			// "miniRamp",
+			// "bmxParkMen",
+			// "bmxParkWomen",
+			// "bmxStreet",
+			// "flatLand",
+			// "mtbDowntownMen",
+			// "mtbDowntownWomen",
+			// "fmbDirtJump",
+			// "breakDance",
+			// "basket3x3",
+		];
 
-    // console.log("reverseData", dataReversed);
+		console.log('reverseData', dataReversed);
 
-    return (
-      <>
-        {dataReversed.map(
-          (yearResults, i) =>
-            yearResults.year > 2012 && (
-              <CellStyle key={yearResults.year}>
-                <TitleCell>{yearResults.year}</TitleCell>
-                <ImgSt
-                  src={process.env.PUBLIC_URL + yearResults.skateWomenPic}
-                  alt="*"
-                />
-                <Name>{yearResults.skateWomen}</Name>
-              </CellStyle>
-            )
-        )}
-      </>
-    );
-  };
-  return <GridStyle>{dataReversed && generateCells()}</GridStyle>;
+		return (
+			<>
+				{dataReversed.map(
+					(yearResults, i) =>
+						yearResults.year > 2001 && (
+							<CellStyle key={yearResults.year}>
+								<TitleCell>{yearResults.year}</TitleCell>
+								<ImgSt src={process.env.PUBLIC_URL + yearResults.skateMenPic} alt='*' />
+								<Name>{yearResults.skateMen}</Name>
+							</CellStyle>
+						)
+				)}
+			</>
+		);
+	};
+	return <GridStyle>{dataReversed && generateCells()}</GridStyle>;
 };
