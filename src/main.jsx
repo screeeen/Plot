294fb1611065e4a2ec01674fbae108ff5d9be3d1
Miguel Alvarez <miguel@screenimplosion.com>
2025-06-01 21:44:13 +0200

    complete

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,6 @@ import { Movi } from './Movi';
 
 
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <Movi />
-//   </React.StrictMode>
-// )
-
 ReactDOM.render(
   <React.StrictMode>
     <Movi />
